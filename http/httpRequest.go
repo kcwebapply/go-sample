7d5449a14129838d1cf881088851a3f796cb3583
kcaiueo <kcwebapply@gmail.com>
2018-12-27 16:04:43 +0900

    echo request

--- http/httpRequest.go
+++ http/httpRequest.go
@@ -13,12 +13,20 @@ func GetTranslate(word string) string {
 	var config Config = GetConfig()
 	url := config.Http.HOST + "?key=" + config.Http.APIKey + "&large_area=Z011&format=json"
 	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Accept", "application/json")
+
 	client := new(http.Client)
-	fmt.Println("http request ")
 	resp, _ := client.Do(req)
-	fmt.Println(resp)
+
 	byteArray, _ := ioutil.ReadAll(resp.Body)
 	js, _ := simplejson.NewJson(byteArray)
-	fmt.Println("responseBody:", js.Get("results").Get("shop").GetIndex(0).Get("name_kana"))
+
+	// get loop size
+	var array, _ = js.Get("results").Get("shop").Array()
+	arraysize := len(array)
+
+	for i := 0; i < arraysize; i++ {
+		fmt.Println(fmt.Sprintf("店名%d,%s", i, js.Get("results").Get("shop").GetIndex(i).Get("name_kana")))
+	}
 	return "ok"
 }
